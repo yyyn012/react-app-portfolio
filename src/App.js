c5fc8733749a51b7a4057b3113d1693a9d4e2424
yyyn012 <yyyn12_@naver.com>
2024-06-23 14:04:46 +0900

    useEffect 2

--- src/App.js
+++ src/App.js
@@ -8,9 +8,19 @@ function App() {
   const onChange = (event) => setKeyword(event.target.value);
 
   console.log("run all the time");
+  // useEffect를 사용하지 않으면 불필요한 코드 실행이 반복된다.
+
   useEffect(() => {
     console.log("CALL THE API...");
   }, []);
+  // useEffect를 사용하여 단 한번만 코드가 실행되도록 설정했다.
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+  // useEffect를 사용하여 keyword가 5글자 이상으로 변화할 때만 코드가 실행되도록 설정했다.
   return (
     <div>
       <input
