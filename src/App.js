9f2ce5c745e0bf0cba7dcabe85c4fba51f554e94
yyyn012 <yyyn12_@naver.com>
2024-06-23 14:36:10 +0900

    text toggle button

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-
-  console.log("hi");
-  // useEffect를 사용하지 않으면 불필요한 코드 실행이 반복된다.
-
-  useEffect(() => {
-    console.log("only once");
-  }, []);
-  // useEffect를 사용하여 단 한번만 코드가 실행되도록 설정했다.
-
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("keyword change");
-    }
-  }, [keyword]);
-  // useEffect를 사용하여 keyword가 5글자 이상으로 변화할 때만 코드가 실행되도록 설정했다.
-
-  const onClick = () => {
-    setCounter((prev) => prev + 1);
-  };
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      ></input>
-      <h1 onChange={onChange}>{counter}</h1>
-      <button onClick={onClick}>click</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
