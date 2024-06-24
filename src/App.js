06fefdfbd8ed779dc778f6a8f34fb498635cb569
yyyn012 <yyyn12_@naver.com>
2024-06-24 17:37:27 +0900

    todos(Adding another array to an array)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [writeTodo, setWriteTodo] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+
+    setWriteTodo((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+
+    // state(constant)를 직접적으로 설정할 수 없기 때문에 modifier function을 통해 state를 관리해야 한다.
+
+    // 리액트에서는 state.push()를 이용할 수 없기 때문에 어떤 배열에 새로운 배열의 element들을 추가하려면 위와 같이 "...[새로운 배열]"등의 방법을 이용해야 한다.
+  };
+  console.log(writeTodo);
   return (
     <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({writeTodo.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
