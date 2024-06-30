4f94aae00117a24a64766d43343352758b00e05b
yyyn012 <yyyn12_@naver.com>
2024-06-30 17:20:38 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,45 +1,34 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [writeTodo, setWriteTodo] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  // fetch()의 response로 입력된 api(리소스의 경로)의 응답을 받고 then()을 사용해 response의 json부분(응답 본문 텍스트)만 추출했다.
 
-    setWriteTodo((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-
-    // state(constant)를 직접적으로 설정할 수 없기 때문에 modifier function을 통해 state를 관리해야 한다.
-
-    // 리액트에서는 state.push()를 이용할 수 없기 때문에 어떤 배열에 새로운 배열의 element들을 추가하려면 위와 같이 "...[새로운 배열]"등의 방법을 이용해야 한다.
-  };
-  console.log(writeTodo);
+  // then() 메서드는 Promise를 리턴하고 두 개의 콜백 함수를 인수로 받는다. 하나는 Promise가 이행했을 때, 다른 하나는 거부했을 때를 위한 콜백 함수이다.
   return (
     <div>
-      <h1>My To Dos ({writeTodo.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {writeTodo.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.simbol}) : ${coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
-    // map()가 key를 필요로 하는 이유는 모든 item을 인식해버리기 때문이다.
   );
 }
 
