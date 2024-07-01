9639051b5efa7827d8735b39e2c318fe025dfaac
yyyn012 <yyyn12_@naver.com>
2024-07-01 15:55:22 +0900

    using then() - Movie App (1)

--- src/App.js
+++ src/App.js
@@ -2,30 +2,18 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    )
       .then((response) => response.json())
       .then((json) => {
+        setMovies(json.data.movies);
         setLoading(false);
-        setCoins(json);
       });
   }, []);
-  return (
-    <div>
-      <h1>The Coins ({coins.length})</h1>
-      {loading ? <strong>Loading</strong> : null}
-
-      <select>
-        {coins.map((coin) => (
-          <option>
-            {coin.name}({coin.simbol}) : ${coin.quotes.USD.price}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
+  return <div>{loading ? <h1>Loading</h1> : null}</div>;
 }
 
 export default App;
