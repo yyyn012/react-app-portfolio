208354e31f2b654e979c5b9e6e6b54e7fb5dd33f
yyyn012 <yyyn12_@naver.com>
2024-07-02 21:02:40 +0900

    Movie Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Movie from "./Movie";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -24,16 +25,12 @@ function App() {
       {loading ? <h1>Loading :)</h1> : null}
       <div>
         {movies.map((movie) => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres.map((g) => (
-                <li key={g}>{g}</li>
-              ))}
-            </ul>
-          </div>
+          <Movie
+            coverImg={movie.medium_cover_image}
+            title={movie.title}
+            summary={movie.summary}
+            genres={movie.genres}
+          />
         ))}
       </div>
     </div>
