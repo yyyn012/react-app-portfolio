e704ccaa1f36906b57b3e8a85fd2f96e55364fc5
yyyn012 <yyyn12_@naver.com>
2024-07-02 19:05:09 +0900

    MovieApp practice

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+
   const getMovies = async () => {
     const json = await (
       await fetch(
         "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
       )
     ).json();
+
     setMovies(json.data.movies);
     setLoading(false);
   };
+
   useEffect(() => {
     getMovies();
   }, []);
@@ -19,7 +22,7 @@ function App() {
   return (
     <div>
       {loading ? (
-        <h1>Loading</h1>
+        <h1>Loading...</h1>
       ) : (
         <div>
           {movies.map((movie) => (
