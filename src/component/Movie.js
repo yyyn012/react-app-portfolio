e9f1b43ea2ab3b9765e367c3fabab7877d43eccd
yyyn012 <yyyn12_@naver.com>
2024-07-02 22:35:15 +0900

    react DOM for make MoviePage (2)

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -14,6 +17,8 @@ function Movie({ coverImg, title, summary, genres }) {
     </div>
   );
 }
+
+// 새로고침 없이 링크를 이동하고 싶다면 a태그 대신 Lick와 그 속성인 to를 이용하면 된다.
 
 Movie.propTypes = {
   coverImg: PropTypes.string.isRequired,
